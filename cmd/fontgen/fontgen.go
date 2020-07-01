c4a6fac5e6fc43f99e3ef1cc731c5c22e10d7ec2
Dustin L. Howett <dustin@howett.net>
2020-06-30 23:46:37 -0700

    Fix font generation where width >=16 by reworking the chargen logic
    This this pull request rewrites the character generator outer loop to be
    based on which uint8 the character starts in, rather an an "index +
    subunit." They're largely fungible, but there's a straightforward
    transformation from u8idx to u32 position plus subunit.
    
    The original code had an issue where it would preferentially store
    glyphs in the 0- and 3- subunits when the font width was a multiple of 8
    pixels. See Examples below for more info.
    
    I also made it preallocate the entire character data array so we don't
    need to conditionally append or update.
    
    For ease of understanding (and reproduceability!) I made it sort the
    character map before inserting it into the packed bitset, and added a
    huge block comment explaining how pixel font packing works.
    
    This change in packing logic didn't require an equivalent change in the
    font renderer, and I've tested and verified the generated fonts in a
    couple applications.
    
    I ran a quick test with fonts of width 7, 8, 9, 16, 17 and 32. Glyph "A"
    is a repeating "XXX___" pattern, glyph "B" is "XX__XX__" and glyph "C"
    is "X_X_X_"
    
    ```
    width_07.txt
    _________CCCCCCC_BBBBBBB_AAAAAAA
    00000000010101010011001101000111
    
    width_08.txt
    ________CCCCCCCC________BBBBBBBB
    00000000010101010000000000110011
    ________________________AAAAAAAA
    00000000000000000000000011000111
    
    width_09.txt
    _______CCCCCCCCC_______BBBBBBBBB
    00000001010101010000000100110011
    _______________________AAAAAAAAA
    00000000000000000000000111000111
    
    width_16.txt
    AAAAAAAA&&&&&&&&CCCCCCCCCCCCCCCC
    11000111000000000101010101010101
    ________________BBBBBBBBBBBBBBBB
    00000000000000000011001100110011
    
    width_17.txt
    BBBBBBBB&&&&&&&AAAAAAAAAAAAAAAAA
    00110011000000000111000111000111
    _______________CCCCCCCCCCCCCCCCC
    00000000000000010101010101010101
    
    width_32.txt
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
    11000111000111000111000111000111
    BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
    00110011001100110011001100110011
    CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
    01010101010101010101010101010101
    ```
    
    The bits marked with `&` were erroneously skipped, and the 16/17-width
    fonts had glyphs A and B cut off (respectively). The 8-pixel-wide font
    was packed sparsely (at approximately double the space it should
    require)
    
    ```
    width_07.txt
    _________CCCCCCC_BBBBBBB_AAAAAAA
    00000000010101010011001101000111
    
    width_08.txt
    ________CCCCCCCCBBBBBBBBAAAAAAAA
    00000000010101010011001111000111
    
    width_09.txt
    _______BBBBBBBBB_______AAAAAAAAA
    00000001001100110000000111000111
    _______________________CCCCCCCCC
    00000000000000000000000101010101
    
    width_16.txt
    BBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAA
    00110011001100110111000111000111
    ________________CCCCCCCCCCCCCCCC
    00000000000000000101010101010101
    
    width_17.txt
    _______________AAAAAAAAAAAAAAAAA
    00000000000000000111000111000111
    _______________BBBBBBBBBBBBBBBBB
    00000000000000010011001100110011
    _______________CCCCCCCCCCCCCCCCC
    00000000000000010101010101010101
    
    width_32.txt
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
    11000111000111000111000111000111
    BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
    00110011001100110011001100110011
    CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
    01010101010101010101010101010101
    ```

--- cmd/fontgen/fontgen.go
+++ cmd/fontgen/fontgen.go
@@ -27,6 +27,7 @@ import (
 	"io/ioutil"
 	"os"
 	"regexp"
+	"sort"
 	"strings"
 	"unicode/utf8"
 
@@ -61,29 +62,67 @@ func generatePixFont(name string, w, h int, v bool, d map[rune]map[int]string) {
 			Font.SetVariableWidth(%t)
 		}
 	`
-	encoded := []uint32{}
 	cm := make(map[rune]uint16)
+
+	// Sort the glyph list so the representation is stable across different invocations
+	// of fontgen.
+	chs := make([]int, 0, len(d))
+	for ch, _ := range d {
+		chs = append(chs, int(ch))
+	}
+	sort.IntSlice(chs).Sort()
 
 	// convert from simple character encoding to packed bitfield
 	// NB fonts should be at most 32 pixels wide to fit in the uint32
 	//    (height is limited to uint8 255)
-	idx := 0
-	sub := -1 - (w / 8)
-	for c, matrix := range d {
-		sub += 1 + (w / 8)
-		if sub > 3 {
-			sub = 0
-			idx = len(encoded)
-		}
+	//
+	// This packed representation stores 1-4 glyphs in a single uint32 (per line).
+	// Each uint32 is trested as four individually-addressable uint8s, and each uint8
+	// can be chosen as the start of a glyph line. Glyphs are stored with their
+	// leftmost pixels in the LSB of a W-bit wide region aligned to the nearest 8 bits.
+	// Glyphs that will not fit are bumped to the next u32.
+	//
+	// That is to say, a 4-character font with an 8-pixel width will be stored
+	// one u32 per line, and a 4-character font with a 9-pixel width will be
+	// stored 2 u32 per line (since you cannot fit >2 9-bit integers in a single
+	// u32 and maintain the 8-bit alignment guarantee)
+	//
+	// Lines are stored in consecutive uint32s.
+	//
+	//         24      16       8       0
+	//          |       |       |       |
+	// 0     DDDD    CCC     BBBB     A   == 0b00001111000011100000111100000100 == 0xf0e0f04
+	// 1    D   D   C   C   B   B    A A  == 0b00010001000100010001000100001010 == 0x1111110a
+	// 2    D   D       C    BBBB   A   A == 0b00010001000000010000111100010001 == 0x11010f11
+	// 3    D   D   C   C   B   B   AAAAA == 0b00010001000100010001000100011111 == 0x1111111f
+	// 4     DDDD    CCC     BBBB   A   A == 0b00001111000011100000111100010001 == 0xf0e0f11
+	// 5                            EEEEE == 0b00000000000000000000000000011111 == 0x1f
+	// 6                                E == 0b00000000000000000000000000000001 == 0x1
+	// 7                             EEEE == 0b00000000000000000000000000001111 == 0xf
+	// 8                                E == 0b00000000000000000000000000000001 == 0x1
+	// 9                            EEEEE == 0b00000000000000000000000000011111 == 0x1f
 
-		cm[c] = uint16(sub) | (uint16(idx) << 2)
+	u8PerCh := uint16((w-1)>>3) + 1 // 0-8 take up 1 byte, 9-16 take up 2, 17-24 take up 3, 24+ take up 4
+	chPerU32 := 4 / u8PerCh         // we can fit 4, 2 or 1 glyphs per u32
+	spacing := 4 / chPerU32         // we must skip 1, 2, or 4 8-bit units between each glyph start
+
+	costPerLine := (len(d) + int(chPerU32/2)) / int(chPerU32) // #u32 per horizontal line in font
+	costTotal := h * costPerLine                              // #u32s required for the whole font
+
+	encoded := make([]uint32, costTotal)
+
+	// i8 tracks the number of 8-bit units we've skipped
+	var i8 uint16
+	for _, c := range chs {
+		matrix := d[rune(c)]
+
+		i32 := int(i8>>2) * h  // i32 is the index into encoded for the u32 for this char
+		dist := int(i8 & 0b11) // how many u8 units into the u32 we're offset
+		cm[rune(c)] = uint16((i32 << 2) | dist)
+
 		for y := 0; y < h; y++ {
-			var line uint32
-			var b uint32 = 1
-			if sub != 0 {
-				line = encoded[idx+y]
-				b <<= uint(8 * sub)
-			}
+			line := encoded[i32+y]
+			var b uint32 = 1 << uint(8*dist)
 
 			if ld, hasLine := matrix[y]; hasLine {
 				for x := 0; x < w; x++ {
@@ -94,12 +133,10 @@ func generatePixFont(name string, w, h int, v bool, d map[rune]map[int]string) {
 				}
 			}
 
-			if sub == 0 {
-				encoded = append(encoded, line)
-			} else {
-				encoded[idx+y] = line
-			}
+			encoded[i32+y] = line
 		}
+
+		i8 += spacing
 	}
 
 	fnt := pixfont.NewPixFont(uint8(w), uint8(h), cm, encoded)
